d7f9c624838b917953c0ef3f6c425da3345e69e0
Robtisqm <finitignat@gmail.com>
2026-06-24 02:22:14 +0500

    Add board configuration settings for Exynos 4412

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,1 +1,27 @@
-#NOTHING :)
+# Настройки архитектуры процессора (Exynos 4412 / Cortex-A9)
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+# Специфика платформы
+TARGET_BOARD_PLATFORM := exynos4
+TARGET_SOC := exynos4412
+TARGET_BOOTLOADER_BOARD_NAME := smdk4412
+
+# Проверка устройства при прошивке
+TARGET_OTA_ASSERT_DEVICE := c0,p4noterf,p4noterfxx,n8000,GT-N8000
+
+# Флаги ядра (Kernel)
+# Указываем компилятору использовать твой дефконфиг
+TARGET_KERNEL_CONFIG := twrp_n80xx_defconfig
+# Для старых устройств Exynos часто требуется кастомный компилятор или обертка ядра
+TARGET_USES_EXYNOS_4412_KERNEL := true
+
+# Флаги TWRP / OrangeFox
+RECOVERY_VARIANT := twrp
+TARGET_RECOVERY_DEVICE_MODULES := true
+TW_THEME := landscape_mdpi
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255

9d5a5c31152bf8f79eca3aba9d129ec1214c6e37
Robtisqm <finitignat@gmail.com>
2026-06-25 00:32:52 +0500

    Add TWRP product makefile and update lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,9 @@
 PRODUCT_MAKEFILES := \
-    device/samsung/p4noterf/omni_p4noterf.mk
+    device/samsung/p4noterf/omni_p4noterf.mk \
+    device/samsung/p4noterf/twrp_p4noterf.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_p4noterf-eng \
-    omni_p4noterf-userdebug \
     twrp_p4noterf-eng \
-    twrp_p4noterf-userdebug
+    twrp_p4noterf-userdebug \
+    omni_p4noterf-eng \
+    omni_p4noterf-userdebug

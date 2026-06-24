c1adb1c9963d24fc95d90161728e10c2b3d598d0
Robtisqm <finitignat@gmail.com>
2026-06-24 23:33:44 +0500

    Add product makefiles and lunch choices for p4noterf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,11 @@
-LOCAL_DIR := $(GET_LOCAL_DIR)
+PRODUCT_MAKEFILES := \
+    device/samsung/p4noterf/omni_p4noterf.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_p4noterf-eng \
+    omni_p4noterf-userdebug \
+    twrp_p4noterf-eng \
+    twrp_p4noterf-userdebugLOCAL_DIR := $(GET_LOCAL_DIR)
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_p4noterf.mk

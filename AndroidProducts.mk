2a20481c462a16e882264135b5ffb75b058d104a
Robtisqm <finitignat@gmail.com>
2026-06-25 00:51:32 +0500

    Clean up AndroidProducts.mk by removing TWRP entries
    Removed TWRP product makefiles and lunch choices.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,6 @@
 PRODUCT_MAKEFILES := \
-    device/samsung/p4noterf/omni_p4noterf.mk \
-    device/samsung/p4noterf/twrp_p4noterf.mk
+    device/samsung/p4noterf/omni_p4noterf.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_p4noterf-eng \
-    twrp_p4noterf-userdebug \
     omni_p4noterf-eng \
     omni_p4noterf-userdebug

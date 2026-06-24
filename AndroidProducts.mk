c79d5382fc662008b77398d74d7c8f11fcf7c9a0
Robtisqm <finitignat@gmail.com>
2026-06-24 22:40:27 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,8 @@
-#
-# Copyright (C) 2026 The OrangeFox Recovery Project
-#
+LOCAL_DIR := $(GET_LOCAL_DIR)
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_p4noterf.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_p4noterf-eng
+    omni_p4noterf-eng \
+    omni_p4noterf-userdebug

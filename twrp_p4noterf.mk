d4ec641ae8416d0cab1063e4781c30f292e11bd6
Robtisqm <finitignat@gmail.com>
2026-06-24 18:07:12 +0500

    Modify OrangeFox Recovery settings for GT-N8000
    Updated device name and added boot logo settings.

--- twrp_p4noterf.mk
+++ twrp_p4noterf.mk
@@ -1,30 +1,33 @@
-# OrangeFox Recovery for Samsung GT-N8000 (p4noterf)
+# OrangeFox Recovery for GT-N8000 (p4noterf)
 
 PRODUCT_DEVICE := p4noterf
 PRODUCT_NAME := twrp_p4noterf
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy Note 10.1
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_RELEASE_NAME := p4noterf
 
 DEVICE_PATH := device/samsung/p4noterf
 
-# Базовое наследование
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# OrangeFox / TWRP флаги
+# ================== OrangeFox Settings ==================
 OF_USE_MAGISK_ZIP := 1
 OF_DISABLE_MIUI_SPECIFIC_FEATURES := 1
 OF_AB_DEVICE := 0
 OF_NO_TREBLE := 1
-OF_NO_BOOTLOGO := 1
-OF_SCREEN_DIMENSIONS := 1280x800
 OF_FL_PATH := /sdcard
 OF_USE_TAR := 1
 OF_USE_TWRP := 1
 OF_KEEP_DM_VERITY := 1
 OF_KEEP_FORCEENCRYPT := 1
 
-# Наследуем основные конфиги устройства
+# Логотип
+OF_NO_BOOTLOGO := 0
+OF_BOOT_LOGO := 1
+TW_THEME := landscape_hdpi
+
+# Разрешение экрана (очень важно)
+TARGET_SCREEN_WIDTH := 1280
+TARGET_SCREEN_HEIGHT := 800
+
+# Наследование
 $(call inherit-product-if-exists, $(DEVICE_PATH)/n8000.mk)
 $(call inherit-product-if-exists, $(DEVICE_PATH)/lineage.mk)

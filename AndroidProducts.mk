e98d1660d4314c60e52f9ba361b7066ee09a0db0
Robtisqm <finitignat@gmail.com>
2026-06-24 23:35:56 +0500

    Refactor AndroidProducts.mk to remove duplicates
    Removed duplicate COMMON_LUNCH_CHOICES entries and fixed formatting.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,11 +5,4 @@ COMMON_LUNCH_CHOICES := \
     omni_p4noterf-eng \
     omni_p4noterf-userdebug \
     twrp_p4noterf-eng \
-    twrp_p4noterf-userdebugLOCAL_DIR := $(GET_LOCAL_DIR)
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_p4noterf.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_p4noterf-eng \
-    omni_p4noterf-userdebug
+    twrp_p4noterf-userdebug

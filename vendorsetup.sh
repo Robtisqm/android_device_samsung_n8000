c39f0fa3715cec687f2760621dc5daa1ce20eada
Robtisqm <finitignat@gmail.com>
2026-06-25 21:23:25 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ echo "" > frameworks/native/libs/binder/libbinder.map
 sed -i 's/version_script: "libbinder.map",//g' frameworks/native/libs/binder/Android.bp
 echo "LIBBINDER { global: *; };" > frameworks/native/libs/binder/libbinder.map
 sed -i '/version_script: "libbinder.map",/d' frameworks/native/libs/binder/Android.bp
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_MISSING_OUTPUTS=true

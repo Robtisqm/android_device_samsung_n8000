21e543aec9cfa576e05acda17b6ce4b9455feb74
Robtisqm <finitignat@gmail.com>
2026-06-25 21:16:27 +0500

    Update vendorsetup.sh to clean version checks
    Clear version check in Android source before Soong starts.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,4 @@
-# This file is placeholder for Android 12+
-# For manual building use: lunch omni_p4noterf-eng
 #!/bin/bash
 
-# Этот скрипт выполняется ДО старта Soong и физически чистит проверку версий в исходниках Android
+echo "" > frameworks/native/libs/binder/libbinder.map
 sed -i 's/version_script: "libbinder.map",//g' frameworks/native/libs/binder/Android.bp

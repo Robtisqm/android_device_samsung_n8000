e9ba74fd27e65ce6d30d4e1ed46421d3c5ad5f5e
Robtisqm <finitignat@gmail.com>
2026-06-25 17:37:04 +0500

    Fix libbinder error in BoardConfig.mk
    Updated libbinder version script and modified Android.bp.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,3 +68,4 @@ TW_EXCLUDE_OPENAES := true
 # Fix for libbinder error
 LIBBINDER_VERSION_SCRIPT := frameworks/native/libs/binder/libbinder.arm32.map
 PRODUCT_PACKAGES += libbinder
+$(shell sed -i 's/version_script: "libbinder.map"/version_script: ""/g' frameworks/native/libs/binder/Android.bp)

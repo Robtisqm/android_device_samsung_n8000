bdb204bddb5e1eb01dbab61a7ad4ae9f950de743
Robtisqm <finitignat@gmail.com>
2026-06-25 20:25:48 +0500

    Update vendorsetup.sh for Android build process
    Add shebang and clean version check in Android sources

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
 # This file is placeholder for Android 12+
 # For manual building use: lunch omni_p4noterf-eng
+#!/bin/bash
+
+# Этот скрипт выполняется ДО старта Soong и физически чистит проверку версий в исходниках Android
+sed -i 's/version_script: "libbinder.map",//g' frameworks/native/libs/binder/Android.bp
